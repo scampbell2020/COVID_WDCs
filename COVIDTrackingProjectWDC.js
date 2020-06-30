fd009c5aa2aff523f0b6af66240f701c44b98fff
scampbell2020 <67660310+scampbell2020@users.noreply.github.com>
2020-06-30 18:12:07 -0400

    Update COVIDTrackingProjectWDC.js

--- COVIDTrackingProjectWDC.js
+++ COVIDTrackingProjectWDC.js
@@ -3,7 +3,7 @@ var myConnector = tableau.makeConnector();
 myConnector.getSchema = function (schemaCallback) {
 var cols = [
 { id : "date", alias:"Date", dataType : tableau.dataTypeEnum.string },
-{ id : "states", alias: "State",dataType : tableau.dataTypeEnum.string },
+{ id : "state", alias: "State Abbreviation",dataType : tableau.dataTypeEnum.string },
 { id : "totalTestsViral",alias: "Total Viral Tests", dataType : tableau.dataTypeEnum.string },
 { id : "positiveTestsViral",alias: "Positive Viral Tests", dataType : tableau.dataTypeEnum.string }
 ];
@@ -15,14 +15,14 @@ columns : cols
 schemaCallback([tableInfo]);
 };
 myConnector.getData = function(table, doneCallback) {
-$.getJSON("https://covidtracking.com/api/v1/us/daily.json", function(resp) {
+$.getJSON("https://covidtracking.com/api/v1/states/daily.json", function(resp) {
 var feat = resp;
 tableData = [];
 // Iterate over the JSON object
 for (var i = 0, len = feat.length; i < len; i++) {
 tableData.push({
 "date": feat[i]["Date"],
-"states": feat[i]["State"],
+"state": feat[i]["State Abbreviation"],
 "totalTestsViral": feat[i] ["Total Viral Tests"],
 "positiveTestsViral": feat[i] ["Positive Viral Tests"]
 });
